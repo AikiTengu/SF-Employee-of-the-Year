d8a5bb8112ee84c418bd10ae312555a5205165e0
AikiTeng <aiki_tengu@yahoo.com>
2021-03-15 22:06:05 +0200

    Voting apex class implemented

--- force-app/main/default/lwc/votingPage/votingPage.js
+++ force-app/main/default/lwc/votingPage/votingPage.js
@@ -2,6 +2,7 @@ import { LightningElement, track, wire } from "lwc";
 import { reduceErrors } from "c/ldsUtils";
 import { ShowToastEvent } from "lightning/platformShowToastEvent";
 import getCandidates from "@salesforce/apex/CandidateController.getCandidates";
+import setVote from "@salesforce/apex/CandidateController.setVote";
 
 export default class votingList extends LightningElement {
 
@@ -15,7 +16,9 @@ export default class votingList extends LightningElement {
 			let preparedCandidates = [];
 			this.candidates.data.forEach(candidate => {
 				let preparedCandidate = {};
+				preparedCandidate.Id = candidate.Id;
 				preparedCandidate.Candidate_Name = candidate.Name;
+				preparedCandidate.Nomination_Name = candidate.Nomination__r.Name;
 				preparedCandidate.Candidate_FirstName = candidate.CandidateContact__r.FirstName;
 				preparedCandidate.Candidate_LastName = candidate.CandidateContact__r.LastName;
 				preparedCandidate.Candidate_Department = candidate.CandidateContact__r.Department;
@@ -30,7 +33,8 @@ export default class votingList extends LightningElement {
 	}
 
 	@track columns = [
-		{ label: 'Nomination', fieldName: 'Candidate_Name', type: 'text' },
+		{ label: 'Candidate', fieldName: 'Candidate_Name', type: 'text' },
+		{ label: 'Nomination', fieldName: 'Nomination_Name', type: 'text' },
 		{ label: 'First Name', fieldName: 'Candidate_FirstName', type: 'text' },
 		{ label: 'Last Name', fieldName: 'Candidate_LastName', type: 'text' },
 		{ label: 'Department', fieldName: 'Candidate_Department', type: 'text' },
@@ -50,36 +54,35 @@ export default class votingList extends LightningElement {
          let candidateId = event.detail.row.Id;
 			 
 		 let candidateName = event.detail.row.Candidate_Name;
-		console.log(this.candidates.data);
-		  this.dispatchEvent(
-		 	 			new ShowToastEvent({
-		 	 				title: "Voted for",
-		 	 				message: `Candidate: ${candidateName}`,
-		 	 				variant: "success"
-		 	 			})
-		  );
-		}
-		// setActiveContact({ contactToSelectId: contactId })
-		// 	.then(() => {
-		// 		this.dispatchEvent(
-		// 			new ShowToastEvent({
-		// 				title: "Active contact selected",
-		// 				message: `Contact name: ${contactName}`,
-		// 				variant: "success"
-		// 			})
-		// 		);
+		//   this.dispatchEvent(
+		//  	 			new ShowToastEvent({
+		//  	 				title: "Voted for",
+		//  	 				message: `Candidate: ${candidateName}`,
+		//  	 				variant: "success"
+		//  	 			})
+		//   );
+		
+		 setVote({ contactToVoteForId: candidateId })
+			.then(() => {
+				this.dispatchEvent(
+					new ShowToastEvent({
+						title: "Voted for",
+						message: `Candidate: ${candidateName}`,
+						variant: "success"
+					})
+				);
 
-		// 	})
-		// 	.catch((error) => {
-		// 		this.dispatchEvent(
-		// 			new ShowToastEvent({
-		// 				title: `Error selecting active contact ${contactName}`,
-		// 				message: error.body.message,
-		// 				variant: "error"
-		// 			})
-		// 		);
-		// 	});
-	//}
+			})
+			.catch((error) => {
+				this.dispatchEvent(
+					new ShowToastEvent({
+						title: `Error voting for ${candidateName}`,
+						message: error.body.message,
+						variant: "error"
+					})
+				);
+			});
+	}
 
 	 get errors() {
 	 	return this.candidates.error ? reduceErrors(this.candidates.error) : [];
